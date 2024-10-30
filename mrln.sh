2a1da2ad426f4f3ff0d8df922c886372d99f6608
indrek <ikiissel@gmail.com>
2024-10-30 14:53:10 +0200

    update README

--- mrln.sh
+++ mrln.sh
@@ -5,10 +5,10 @@
 ###################################
 
 merlin_dir=$(pwd)
-temp_dir=/home/indrek/mrln_et_light/temp
-voice=eki_et_tnu16k
-in_text=/home/indrek/mrln_et_light/in.txt
-out_wav=/home/indrek/mrln_et_light/out.wav
+temp_dir=/home/indrek/mrln_et_iw/temp
+voice=evas_art_48k
+in_text=/home/indrek/mrln_et_iw/in.txt
+out_wav=/home/indrek/mrln_et_iw/out.wav
 
 echo "synthesizing ..."
 python ${merlin_dir}/src/run_merlin.py ${merlin_dir} ${temp_dir} ${voice} ${in_text} ${out_wav}
